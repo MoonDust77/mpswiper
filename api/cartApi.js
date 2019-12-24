364902ea611d3e7bf5a2006612e1f9effe6df8c2
qiqi <13759525126@163.com>
2019-12-24 18:03:18 +0800

    加入购物车

--- api/cartApi.js
+++ api/cartApi.js
@@ -3,5 +3,8 @@ import fetch from './fetch'
 export const cartApi = {
   getCartList() {
     return fetch('/cart/load')
+  },
+  add(params = {}) {
+    return fetch('/cart/add', params, 'POST')
   }
 }
---
+++ components/add2Cart/add2Cart.js
@@ -0,0 +1,25 @@
+// components/add2Cart/add2Cart.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+    // 空白函数 禁止滚动
+    preventTouchMove: function (e) {
+    },
+  }
+})
